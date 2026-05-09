1401320e791b74bf9fe930039ea4ed7f3c99d942
igorbasko01 <igorbasko@gmail.com>
2026-05-09 16:52:30 +0300

    Simplify visuals: Remove shaders and complex surfaces, planets are now single-color circles

--- lib/earth.dart
+++ lib/earth.dart
@@ -1,23 +1,11 @@
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
-import 'package:orbitals/procedural_planet_surface.dart';
 
 class Earth extends CircleComponent {
   Earth({required double radius, required Anchor anchor})
-    : super(radius: radius, anchor: anchor);
-
-  @override
-  Future<void> onLoad() async {
-    super.onLoad();
-
-    add(
-      ProceduralPlanetSurface(
-        waterColor: const Color.fromARGB(255, 20, 114, 255),
-        landColor: const Color(0xFF388E3C),
-        rotationSpeed: 0.5,
-      )..priority = 1,
-    );
-
-    // add(CloudLayer()..priority = 2);
-  }
+    : super(
+        radius: radius,
+        anchor: anchor,
+        paint: Paint()..color = const Color(0xFF1E88E5),
+      );
 }
