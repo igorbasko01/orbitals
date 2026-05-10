83b2b596594cd9e296a988c30098f4ea246ecb70
igorbasko01 <igorbasko@gmail.com>
2026-05-10 21:52:29 +0300

    chore: use super parameters in Orbiter and cleanup imports

--- lib/orbiter.dart
+++ lib/orbiter.dart
@@ -1,17 +1,13 @@
-import 'dart:math' as math;
-
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
 class Orbiter extends CircleComponent {
   Orbiter({
-    required double radius,
+    required super.radius,
     required Color color,
-    Vector2? position,
+    super.position,
   }) : super(
-         radius: radius,
          anchor: Anchor.center,
          paint: Paint()..color = color,
-         position: position,
        );
 }
